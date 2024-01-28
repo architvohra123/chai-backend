41644b591c9a136f964abe790e7e258594af8ba9
architvohra123 <archit0249.be21@chitkara.edu.in>
2024-01-28 11:46:52 +0530

    lets start

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Backend Project
+
+This is a Backend Project written in javascript based on the Youtube tutorial of Chai Aur Code Backend Series.
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+//making asynchandler using promises resolve reject block
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+//making asyncHandler using try/catch block
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch(err){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
