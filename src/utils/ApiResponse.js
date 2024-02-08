f42e33e9c843428754616c1a73aed514e54574cd
architvohra123 <archit0249.be21@chitkara.edu.in>
2024-02-09 00:18:49 +0530

    added register controller and bug fixes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,4 +1,4 @@
-class ApiRespons {
+class ApiResponse {
     constructor(statusCode, data, message="Success"){
         this.statusCode = statusCode
         this.data = data
@@ -6,3 +6,5 @@ class ApiRespons {
         this.success = statusCode < 400
     }
 }
+
+export {ApiResponse}
